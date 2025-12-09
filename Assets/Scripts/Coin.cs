3be1c3d5d465e5a4e09d12c3c85b7eb1c03c366a
esv02 <emma.sun@student.sl.on.ca>
2025-12-08 22:53:53 -0500

    Added CoinUI

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
+    public int coinsToGive = 1;
+    private TextMeshProUGUI coinText;
+
+    private void Start()
+    {
+        coinText = GameObject.FindWithTag("CoinText").GetComponent<TextMeshProUGUI>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-            player.coins += 1;
+            player.coins += coinsToGive;
+            coinText.text = player.coins.ToString();
             Destroy(gameObject);
 
             // Play coin sound
